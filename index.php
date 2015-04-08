8a500f91e259f9f455dd11bd91951b2bb38c6853
Jędrzej Skrzypczak <yurciu@gmail.com>
2015-04-08 16:10:55 +0200

    Dodane haki tha

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
-<?php get_header();
-
-
-?>
-
+<?php get_header(); ?>
+<?php tha_content_before(); ?>
 <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12 col-lg-push-2 col-md-push-2 col-sm-push-3">
+<?php tha_content_top(); ?>
 <?php
       getMainContentView(45);
 
@@ -13,7 +11,10 @@
 
       getMainContentView(44);
 ?>
+<?php tha_content_bottom(); ?>
 </div>
+<?php tha_content_after(); ?>
+
 <?php get_sidebar('left'); ?>
 <?php get_sidebar('right'); ?>
 
