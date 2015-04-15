79c0467332ae2049221fa27534acb3ea126b0851
Jędrzej Skrzypczak <yurciu@gmail.com>
2015-04-15 22:13:53 +0200

    [refactoring] Formatowanie kodu

--- category.php
+++ category.php
@@ -3,22 +3,23 @@
 
 <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12 col-lg-push-2 col-md-push-2 col-sm-push-3">
         <div class="news news-list">
+        <h1 class='text-center'><?php single_cat_title( '', true );?></h1> <hr />
 
-<h1 class='text-center'><?php single_cat_title( '', true );?></h1> <hr />
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class='row'>
+            <div class="col-lg-3 col-md-3 col-sm-5">
+            <?php echo the_post_thumbnail('thumbnail', array('class' => 'thumbnail pull-left img-responsive') ); ?>
+            </div>
+            <div class="col-lg-9 col-md-9 col-sm-7">
+            <h2>
+                <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+            </h2>
+            <?php the_time('j F Y') ?>
+        </div>
 
-<div class='row'>
-    <div class="col-lg-3 col-md-3 col-sm-5">
-    <?php echo the_post_thumbnail('thumbnail', array('class' => 'thumbnail pull-left img-responsive') ); ?>
-    </div>
-    <div class="col-lg-9 col-md-9 col-sm-7">
-    <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-    <?php the_time('j F Y') ?>
-
-    </div>
-
-    <div class='clearfix'></div><hr>
+        <div class='clearfix'></div>
+        <hr>
 </div>
 
 
--- page.php
+++ page.php
@@ -2,36 +2,25 @@
 
 <?php tha_content_before() ?>
 <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12 col-lg-push-2 col-md-push-2 col-sm-push-3">
-        <article class="news news-list">
+        <div class="news news-list">
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="entry">
-<?php the_post_thumbnail(); ?>
-
-<h1 class='text-center'><?php the_title(); ?></h1><hr>
-
-<a href="<?php the_permalink(); ?>">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'single-post', array('class' => 'entry-thumb') ); } ?>
-</a>
-<?php the_content('Czytaj więcej &rarr;'); ?>
-
-
-<div class="clear"></div>
-</div> <!-- end entry -->
-
-
-
-<?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
-<!-- <div class=”commentsblock”>
-<?php //comments_template(); ?>
-</div> -->
-
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <div class="entry">
+                <?php the_post_thumbnail(); ?>
+                <h1 class='text-center'><?php the_title(); ?></h1>
+                <hr>
+                <a href="<?php the_permalink(); ?>">
+                <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'single-post', array('class' => 'entry-thumb') ); } ?>
+                </a>
+                <?php the_content('Czytaj więcej &rarr;'); ?>
+                <div class="clear"></div>
+            </div>
+            <?php endwhile; else: ?>
+            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+            <?php endif; ?>
           <div class="clearfix"></div>
-        </article>
-
-      </div>
+        </div>
+</div>
 <?php get_sidebar('left'); ?>
 <?php get_sidebar('right'); ?>
 <?php get_footer(); ?>
